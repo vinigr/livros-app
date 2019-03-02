2d3b03876c997132bd84fcbca9b9491c02d48df1
= <=>
2019-03-01 23:52:12 -0300

    Criação do cadastro

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -2,25 +2,37 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import appLogo from '../img/logo.png'; 
+import './Cadastro.css';
 
 class Cadastro extends Component{
     render(){
         return(
-            <div className='login-wrapper'>
-                <img src={appLogo} className='logo' alt='Meus Livros'/>
-                <form className='form'>
+            <div className='cadastro'>
+                <header>
+                    <img src={appLogo} className='logo' alt='Meus Livros'/>
+                    <div className='login-entrar'>
+                        <h3>Já possui conta?</h3>
+                        <Link to='/login'><button>Entre</button></Link>
+                    </div>
+                    
+                </header>
+                
+
+
+
+                {/* <form className='form'>
                     <h1>Cadastro</h1>
-                    <input type='name' placeholder='Nome completo' />
-                    <input type='email' placeholder='E-mail' autoFocus/>
+                    <input type='name' placeholder='Nome completo' autoFocus />
+                    <input type='email' placeholder='E-mail' />
                     <input type='password' placeholder='Senha'/>
-                    <input type='date' placeholder='Data de Nascimento' />
+                    <label>Data de nascimento:</label>
+                    <input type='date' placeholder='Data de Nascimento' className='data'/>
                     <input type='submit' value='Cadastrar' />
                     <hr />
                     <h3>Conecte-se com:</h3>
                     <input type='submit' className='button-facebook' value='Facebook' />
                     <input type='submit' className='button-google' value='Google'/>
-                </form>
-                <h3 className='cadastro'>Já possui conta? <Link to='/cadastro'><strong>Entre</strong></Link></h3>
+                </form> */}
             </div>
         )
     }
